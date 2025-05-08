4374e9548d56156cdccd6f0b8b727467aac51b88
Teelake <teelake21@gmail.com>
2025-05-08 13:06:44 +0100

    updates

--- backend/api/complete-signup.php
+++ backend/api/complete-signup.php
@@ -8,11 +8,11 @@ header('Content-Type: application/json');
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (empty($data['first_name']) || empty($data['last_name']) || empty($data['email']) || empty($data['password'])) {
+/*if (empty($data['first_name']) || empty($data['last_name']) || empty($data['email']) || empty($data['password'])) {
     http_response_code(400);
     echo json_encode(["status" => "error", "message" => "All fields are required"]);
     exit;
-}
+}*/
 
 $userController = new UserController();
 $response = $userController->finalizeSignup($data);
