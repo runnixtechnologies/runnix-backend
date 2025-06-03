26cef7aefff21d4f4b45a5a2fc9042a4b2a6c420
Teelake <teelake21@gmail.com>
2025-06-03 23:09:55 +0100

    bulk items upload updates

--- backend/api/bulk-create-items.php
+++ backend/api/bulk-create-items.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 
 require_once '../../vendor/autoload.php';
 require_once '../config/cors.php';
+require_once '../middleware/authMiddleware.php'; 
 
 use Controller\ItemController;
 use function Middleware\authenticateRequest;
