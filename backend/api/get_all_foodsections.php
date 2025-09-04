57b3d846d7d6a1c39e25b39a285065aba3ec99a9
Teelake <teelake21@gmail.com>
2025-09-04 10:09:50 +0100

    updates

--- backend/api/get_all_foodsections.php
+++ backend/api/get_all_foodsections.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php-error.log'); 
 require_once '../../vendor/autoload.php';
 require_once '../config/cors.php';
 require_once '../middleware/authMiddleware.php';
