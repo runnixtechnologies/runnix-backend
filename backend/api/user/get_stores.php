807866543358beb4c2e6b53f4d7be751be579675
Teelake <teelake21@gmail.com>
2025-09-29 14:16:29 +0100

    new updates

--- backend/api/user/get_stores.php
+++ backend/api/user/get_stores.php
@@ -2,8 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once '../../vendor/autoload.php';
-require_once '../config/cors.php';
+require_once '../../../vendor/autoload.php';
+require_once '../../config/cors.php';
 
 use Controller\StoreController;
 use Middleware\AuthMiddleware;
